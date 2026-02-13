198c46429b13cc8e0d511d3f0ee93e5018c9e934
Sarnvia Das <sarnivadas@gmail.com>
2026-02-13 20:24:24 +0530

    refactor: improve error handling in middleware and clean up authentication checks

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -11,8 +11,8 @@ export const protectRoute = [
   async (req: AuthRequest, res: Response, next: NextFunction) => {
     try {
       const { userId: clerkId } = getAuth(req);
-      if (!clerkId)
-        return res.status(400).json({ message: "Unauthorized access" });
+      // if (!clerkId)
+      //   return res.status(401).json({ message: "Unauthorized access" });
       const user = await User.findOne({ clerkId });
 
       if (!user) return res.status(404).json({ message: "User not found" });
@@ -20,8 +20,8 @@ export const protectRoute = [
       req.userId = user._id.toString();
       next();
     } catch (error) {
-      console.log("Error in Protected Route", error);
-      return res.status(500).json({ message: "Internal server error" });
+      res.status(500);
+      next(error);
     }
   },
 ];

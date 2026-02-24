e1e2719b730dac496865df89deb63725cdf64bc1
Sarnvia Das <sarnivadas@gmail.com>
2026-02-24 23:15:50 +0530

    feat: set up socket server with user authentication and message handling

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,15 @@
 import app from "./src/app";
 import { connectDB } from "./src/config/database";
+import { createServer } from "http";
+import { initializeSocket } from "./src/utils/socket";
 
+const httpServer = createServer(app);
 const PORT = process.env.PORT || 3000;
+initializeSocket(httpServer);
 
 connectDB()
   .then(() => {
-    app.listen(PORT, () => {
+    httpServer.listen(PORT, () => {
       console.log("The server is up and listening on Port:", PORT);
     });
   })

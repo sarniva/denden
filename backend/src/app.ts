ba165495ed6d7136e7a4caf453d803a2e1956860
Sarnvia Das <sarnivadas@gmail.com>
2026-02-26 18:49:01 +0530

    fix: correct static file path for production environment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -25,7 +25,7 @@ app.use("/api/user", userRoutes);
 app.use(errorHandler);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../..web/dist")));
+  app.use(express.static(path.join(__dirname, "../../web/dist")));
 
   app.get("/{*any}", (_req, res) => {
     res.sendFile(path.join(__dirname, "../../web/dist/index.html"));
